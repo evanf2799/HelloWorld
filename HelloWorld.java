bbfe5b7f67a6106e826ffec5a6146481d291e1c0
evanf2799 <259186632+evanf2799@users.noreply.github.com>
2026-09-18 15:26:59 -0400

    program now asks user for name and outputs personalized greeting

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner s = new Scanner(System.in);
+        System.out.println("What's your name?");
+        String name = s.nextLine();
+        System.out.println("Hello, " + name + "!");
     }
 }
