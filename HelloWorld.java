4afa8234b0e1979430bb1a1e3fb6400549b0f644
evanf2799 <259186632+evanf2799@users.noreply.github.com>
2026-09-18 15:34:12 -0400

    Adds Class.txt and file handling, now prints course name.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,17 @@
 import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 public class HelloWorld {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         Scanner s = new Scanner(System.in);
         System.out.println("What's your name?");
         String name = s.nextLine();
         System.out.println("Hello, " + name + "!");
+        s.close();
+        File f = new File("Class.txt");
+        s = new Scanner(f);
+        String course = s.nextLine();
+        System.out.println("Welcome to " + course + "!");
         s.close();
     }
 }
